c0d8bbae6d42fb354b576dadfe8e98d1b7581cfc
Bhanuch2006 <maheshbhanu.chekuri@gmail.com>
2026-06-03 13:41:22 +0530

    new changes

--- Assets/Scripts/Basic.cs
+++ Assets/Scripts/Basic.cs
@@ -6,7 +6,7 @@ public class Basic : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        //hello world 
     }
 
     // Update is called once per frame
